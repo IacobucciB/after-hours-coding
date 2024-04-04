30671fa49b1dcf23848b5c5aba1334c981b968c3
Iacobucci Juan B <link00222@gmail.com>
2024-04-03 22:28:37 -0300

    Create two-sum.py

---
+++ leetcode/001-two-sum/two-sum.py
@@ -0,0 +1,20 @@
+def twoSum(nums: list[int], target: int) -> list[int]:
+    for i,e in enumerate(nums):
+        print(i,e)
+        
+    return [0,0]
+
+print("Two Sum")
+
+eg1 = [2,7,11,15]
+tgt1 = 9
+print(twoSum(eg1,tgt1))
+
+eg2 = [3,2,4]
+tgt2 = 6
+print(twoSum(eg2,tgt2))
+
+eg3 = [3,3]
+tgt3 = 6
+print(twoSum(eg3,tgt3))
+
