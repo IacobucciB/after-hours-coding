014cc33363a9dcac0b7dc565974f5369ac9436ee
Iacobucci Juan B. <link00222@gmail.com>
2024-05-05 22:20:53 -0300

    Update valid-parentheses.py

--- leetcode/020-valid-parentheses/valid-parentheses.py
+++ leetcode/020-valid-parentheses/valid-parentheses.py
@@ -1,2 +1,24 @@
 def isValid(s: str) -> bool:
-	return False
+    stack = []
+    brackets_map = {
+        ')': '(',
+        ']': '[',
+        '}': '{'
+    }
+    
+    for e in s:
+        if e in brackets_map.values():
+            stack.append(e)
+        elif e in brackets_map.keys():
+            if not stack or stack.pop() != brackets_map[e]:
+                return False
+    return not stack
+
+s = "()"
+print(isValid(s))
+
+s = "()[]{}"
+print(isValid(s))
+
+s = "(]"
+print(isValid(s))
